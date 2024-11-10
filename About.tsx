11daa20e4b10edefe3353df307ffd349d6b5a8b3
sairarizvi6 <164424696+sairarizvi6@users.noreply.github.com>
2024-11-10 16:27:50 +0500

    Add files via upload

---
+++ About.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import '../app/styles/about.css';
+
+const About = () => {
+  return (
+    <div id="about" className="about-container">
+      <h2 className="about-heading" data-aos="zoom-in-up">
+        About Me
+      </h2>
+      <p className="about-text" data-aos="zoom-in-up">
+      As a dedicated student at GIAIC, I am immersed in a comprehensive course covering Artificial Intelligence, Web 3.0, and the Metaverse. 
+      Driven by a passion for technology,I am committed to continuously expanding my expertise and staying at the forefront of innovation in these transformative fields.
+      </p>
+    </div>
+  );
+};
+
+export default About;
